174e97e59c3eea44518cbfcbbbe198c1bdf88e8d
chrisduffey <duffeyvt12@gmail.com>
2024-08-07 18:27:22 -0400

    header.jsx

--- react-portfolio/src/Components/Header.jsx
+++ react-portfolio/src/Components/Header.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiBars4, HiXMark } from "react-icons/hi2";
 
 function Header() {
+    const [toggle, setToggle]=useState(false)
     const menuList= [
         {
             id:1, 
@@ -30,7 +32,7 @@ function Header() {
         <div>
         <h2 className='text-[24px] font-bold text-blue '>Chris <span className='text-red-500'>Duffey</span></h2>
         </div>
-        <div className='flex gap-4'>
+        <div className='hidden md:flex gap-4'>
             {menuList.map((item)=> (
                 <div>
                     <h2 className='text-white 
@@ -39,6 +41,10 @@ function Header() {
             ))}
             <h2 className='text-white 
                     hover: border-[1px] border-blue-500 rounded-full text-[15px] px-3 py-1 cursor-pointer hover:bg-gradient-to-r from-red-500 to-red-800'>Hire Me</h2>
+        </div>
+        <div className='md:hidden'>
+        {!toggle? <HiBars4 onClick={()=>setToggle(!toggle)} className='text-black text-[22px]'/>
+        :<HiXMark onClick={()=>setToggle(!toggle)}  className='text-black text-[22px]'/>}
         </div>
     </div>
     
